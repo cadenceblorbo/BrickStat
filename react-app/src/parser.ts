3ca20358c3fffe7d3f7e2f2ad48b66d4e2619d34
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2025-09-23 21:52:22 -0400

    start data parser

---
+++ react-app/src/parser.ts
@@ -0,0 +1,9 @@
+import csvParser from 'csv-parser';
+import fs from 'fs';
+
+//last data: sept 23 2025
+
+const setsPath = "sets.csv"
+const inventoriesPath = "inventories.csv"
+const inventoryPartsPath = "inventory_parts.csv"
+
