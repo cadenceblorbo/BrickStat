13c242f98e002dbadf16c855208a4e0bb763ef80
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2025-11-04 18:24:38 -0500

    basic react three fiber demo

---
+++ react-app/src/canvas.tsx
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import { useState, useRef } from 'react'
+import { Canvas, useFrame, type ThreeElements } from '@react-three/fiber'
+
+function InteractableBoxMesh(props: ThreeElements['mesh']) {
+    const meshRef = useRef<THREE.Mesh>(null!);
+    const [hover, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+    useFrame((state, delta) => (meshRef.current.rotation.x += delta));
+    return (
+        <mesh
+            {...props}
+            ref={meshRef}
+            scale={active ? 1.5 : 1}
+            onClick={() => setActive(!active)}
+            onPointerOver={() => setHover(true)}
+            onPointerOut={() => setHover(false)}
+        >
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hover ? 'hotpink' : 'blue'}/>
+        </mesh>
+    )
+}
+
+function StatsCanvas() {
+    return (
+        <div>
+            <Canvas>
+                <ambientLight intensity={Math.PI / 2} />
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+                <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+                <InteractableBoxMesh position={[-1.2, 0, 0]} />
+                <InteractableBoxMesh position={[1.2, 0, 0]} />
+            </Canvas>
+        </div>
+    )
+}
+
+export default StatsCanvas;
