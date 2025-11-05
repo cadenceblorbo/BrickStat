7a388e545c0781dbe3159454d8d9b6313072d3c8
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2025-11-04 19:18:15 -0500

    separate files

---
+++ react-app/src/rendering/CameraToy.tsx
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+import { useFrame } from '@react-three/fiber'
+
+function CameraToy() {
+    let dir = 1
+    const min = 20
+    const max = 120
+    const speed = 10
+    useFrame((state, delta) => {
+        if (state.camera instanceof THREE.PerspectiveCamera) {
+            console.log("here");
+            if (state.camera.fov > max) {
+                dir = -1
+            } else if (state.camera.fov < min) {
+                dir = 1
+            }
+            state.camera.fov += delta * dir * speed;
+            state.camera.updateProjectionMatrix();
+        }
+    });
+    return <></>
+}
+
+export default CameraToy
--- react-app/src/rendering/StatsCanvas.tsx
+++ react-app/src/rendering/StatsCanvas.tsx
@@ -1,46 +1,6 @@
-import * as THREE from 'three';
-import { useState, useRef } from 'react'
-import { Canvas, useFrame, type ThreeElements } from '@react-three/fiber'
-
-function InteractableBoxMesh(props: ThreeElements['mesh']) {
-    const meshRef = useRef<THREE.Mesh>(null!);
-    const [hover, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    useFrame((state, delta) => (meshRef.current.rotation.x += delta));
-    return (
-        <mesh
-            {...props}
-            ref={meshRef}
-            scale={active ? 1.5 : 1}
-            onClick={() => setActive(!active)}
-            onPointerOver={() => setHover(true)}
-            onPointerOut={() => setHover(false)}
-        >
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hover ? 'hotpink' : 'blue'}/>
-        </mesh>
-    )
-}
-
-function CameraToy() {
-    let dir = 1
-    const min = 20
-    const max = 120
-    const speed = 10
-    useFrame((state, delta) => {
-        if (state.camera instanceof THREE.PerspectiveCamera) {
-            console.log("here");
-            if (state.camera.fov > max) {
-                dir = -1
-            } else if (state.camera.fov < min) {
-                dir = 1
-            }
-            state.camera.fov += delta * dir * speed;
-            state.camera.updateProjectionMatrix();
-        }
-    });
-    return <></>
-}
+import { Canvas} from '@react-three/fiber'
+import CameraToy from "./CameraToy"
+import InteractableBoxMesh from "./InteractableBoxMesh"
 
 function StatsCanvas() {
     
