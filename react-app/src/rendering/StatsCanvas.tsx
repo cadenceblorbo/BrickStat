dc1ed74bf82d9bf5a0baccc9ffa52d1286248a66
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2025-11-12 19:38:16 -0500

    basic slider

--- react-app/src/rendering/StatsCanvas.tsx
+++ react-app/src/rendering/StatsCanvas.tsx
@@ -4,18 +4,27 @@ import { Canvas } from '@react-three/fiber'
 //import InteractableBoxMesh from "./InteractableBoxMesh"
 import { Histogram3D, type HistogramColumnProps } from "./Histogram3D"
 import CameraControls from "./CameraControls"
-import * as JSONParse from '../json-parser.ts'
 
-function StatsCanvas() {
+export interface StatsCanvasProps {
+    data: { [key: string]: number }
+    xCols: number,
+    yCols: number
+}
+
+export function StatsCanvas({
+    data,
+    xCols,
+    yCols
+}: StatsCanvasProps) {
     const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    cam.position.z = 5
+    cam.position.z = 10
     cam.updateProjectionMatrix();
 
     const mat1 = new THREE.MeshStandardMaterial({ color: 'green' });
     const mat2 = new THREE.MeshStandardMaterial({ color: 'black', side: THREE.DoubleSide});
-    const mat3 = new THREE.MeshStandardMaterial({ color: 'white' });
+    const mat3 = new THREE.MeshStandardMaterial({ color: 'gray' });
 
-    const onDataPresent = (props: HistogramColumnProps, dataVal: unknown, i: number, j: number): void => {
+    const onDataPresent = (props: HistogramColumnProps, dataVal: unknown): void => {
         props.height = Math.max(Math.log(Number(dataVal)), 0.1)
         props.meshProps.material = mat1
     }
@@ -27,29 +36,17 @@ function StatsCanvas() {
             props.meshProps.material = mat3
         }
     }
-    const data: { [key: string]: number } = {}
-    const x = 5
-    const y = 5
-    for (let i = 0; i < x; i++) {
-        for (let j = i; j < y; j++) {
-            data[(i+1)+","+(j+1)] = i + j
-        }
-    }
-    delete data["1,1"]
 
-    const dataT = JSONParse.retrieveData()
-    console.log(dataT.cumulative)
+    
 
     return (
         <div>
             <Canvas camera={cam}>
                 <ambientLight intensity={Math.PI / 2} />
                 <pointLight position={[10, 10, 10]} decay={0} intensity={Math.PI} />
-                <Histogram3D xCols={dataT.xCols} yCols={dataT.yCols} data={dataT.cumulative[2025]} onDataPresent={onDataPresent} onDataAbsent={onDataAbsent} />
+                <Histogram3D xCols={xCols} yCols={yCols} data={data} onDataPresent={onDataPresent} onDataAbsent={onDataAbsent} />
                 <CameraControls/>
             </Canvas>
         </div>
     )
 }
-
-export default StatsCanvas;
