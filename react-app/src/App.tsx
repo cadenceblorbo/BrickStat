c1109ea69cf3bb0330fa5f05d1864e13fe4fd808
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2025-11-17 18:27:05 -0500

    slider buttons

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+﻿import { useState } from 'react'
 import { StatsCanvas } from './rendering/StatsCanvas.tsx'
 import * as JSONParse from './json-parser.ts'
 import * as THREE from 'three';
@@ -7,10 +7,17 @@ import './App.css'
 function App() {
     const data = JSONParse.retrieveData()
 
-    const [sliderVal, setSliderVal] = useState(data.firstYear);
+    const [yearVal, setYearVal] = useState(data.firstYear);
 
-    function handleSliderChange(event: { currentTarget: { value: any; }; }) {
-        setSliderVal(event.currentTarget.value);
+    function sliderYearChange(event: { currentTarget: { value: string; }; }) {
+        setYearVal(Number(event.currentTarget.value));
+    }
+
+    function buttonYearChange(change: number) {
+        const newYear = Math.min(Math.max(yearVal + change, data.firstYear), data.lastYear);
+        if (newYear != yearVal) {
+            setYearVal(newYear);
+        }
     }
 
     const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -22,9 +29,15 @@ function App() {
 
     return (<div>
         <div className = "stats-canvas-parent">
-            <StatsCanvas xCols={data.xCols} yCols={data.yCols} cam={cam} data={data.cumulative[sliderVal]} />
+            <StatsCanvas xCols={data.xCols} yCols={data.yCols} cam={cam} data={data.cumulative[yearVal]} />
         </div>
-        <input type="range" min={data.firstYear} max={data.lastYear} onChange={handleSliderChange} defaultValue={sliderVal}></input>        
+        <div className = "year-controls">
+            <input className="year-slider" type="range" min={data.firstYear} max={data.lastYear} onChange={sliderYearChange} value={yearVal}></input>
+            <button onClick={() => {buttonYearChange(-1)} }>{"←"}</button>
+            <p>{yearVal}</p>
+            <button onClick={() => { buttonYearChange(1) }}>{"→"}</button>
+        </div>
+        
     </div>)
 }
 
