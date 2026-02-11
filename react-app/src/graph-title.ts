4fd8ae00558bab4f1e365a5198d459f11d2bb201
h4ntyum1 <123522477+cadenceblorbo@users.noreply.github.com>
2026-02-10 19:39:54 -0500

    automate title creation

--- react-app/src/graph-title.ts
+++ react-app/src/graph-title.ts
@@ -1,19 +1,46 @@
-const titleMap = new Map([
-    ["Bricks Total Quantity Cumulative", "Cumulative Total Brick Quantities by Size"],
-    ["Bricks Set Apperances Cumulative", "Cumulative Brick Set Apperances by Size"],
-    ["Bricks Total Quantity By Year", "Yearly Total Brick Quantities by Size"],
-    ["Bricks Set Apperances By Year", "Yearly Brick Set Apperances by Size"],
-    ["Plates Total Quantity Cumulative", "Cumulative Total Plate Quantities by Size"],
-    ["Plates Set Apperances Cumulative", "Cumulative Plate Set Apperances by Size"],
-    ["Plates Total Quantity By Year", "Yearly Total Plate Quantities by Size"],
-    ["Plates Set Apperances By Year", "Yearly Plate Set Apperances by Size"],
-]);
+import { ChronoType, PartType, QuantityType } from './utils/lego-enum.ts';
 
-export function GraphTitle(partType: string, quantityType: string, chronoType: string): string {
-    const comb = partType + " " + quantityType + " " + chronoType;
-    if (titleMap.has(comb)){
-        return (titleMap.get(comb));
-    } else {
-        return ("Default Title");
+export function GraphTitle(partType: PartType, quantityType: QuantityType, chronoType: ChronoType): string {
+    let result = "";
+
+    switch (partType) {
+        case PartType.Bricks:
+            result = "Brick";
+            break;
+        case PartType.Plates:
+            result = "Plate";
+            break;
+        case PartType.Tiles:
+            result = "Tile";
+            break;
+        default:
+            result = "Default Part";
+            break;
     }
+
+    switch (quantityType) {
+        case QuantityType.SetApperances:
+            result += " Set Apperances";
+            break;
+        case QuantityType.TotalQuantity:
+            result = "Total " + result + " Quantities";
+            break;
+        default:
+            result += " Default Quantity Type";
+            break;
+    }
+
+    switch (chronoType) {
+        case ChronoType.ByYear:
+            result = "Yearly " + result;
+            break;
+        case ChronoType.Cumulative:
+            result = "Cumulative " + result;
+            break;
+        default:
+            result = "Default Time Format " + result;
+            break;
+    }
+
+    return result + " by Size";
 }
